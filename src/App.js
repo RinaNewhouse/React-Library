22512abfdae779d46c2fc0772a1bc07db488a078
Rina <rina.newhouse@gmail.com>
2025-05-16 15:24:22 -0400

    new commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './index.css';
 import Nav from './components/Nav'
+import Landing from './components/Landing'
+import Highlights from './components/Highlights'
+import Features from './components/Features';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Landing />
+      <Highlights />
+      <Features />
     </div>
   );
 }
