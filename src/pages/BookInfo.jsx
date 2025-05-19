c6418ed6eef386a3672d4da528128e15b46f7a03
Rina <rina.newhouse@gmail.com>
2025-05-19 17:33:32 -0400

    new commit

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -52,6 +52,15 @@ const BookInfo = ({ books }) => {
             </div>
           </div>
         </div>
+        <div className="books__container">
+            <div className="row">
+                <div className="book__selected--top">
+                    <h2 className="book__selected--title--top">
+                        Recommended Books
+                    </h2>
+                </div>
+            </div>
+        </div>
       </main>
     </div>
   );
