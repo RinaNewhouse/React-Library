70cb3e3d9a020096239863d44214a47b2c070510
Rina <rina.newhouse@gmail.com>
2025-05-26 14:33:40 -0500

    new commit

--- src/App.js
+++ src/App.js
@@ -6,15 +6,28 @@ import { books } from "./data";
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import BookInfo from './pages/BookInfo'
+import Cart from './pages/Cart'
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, book])
+  }
+
+  useEffect (() => {
+    console.log(cart)
+  }, [cart])
+
   return (
     <Router>
       <div className="App">
         <Nav />
         <Route path="/" exact component={Home} />
         <Route path="/books" exact render={() => <Books books={books} />} />
-        <Route path="/books/:id" render={() => <BookInfo books={books} />} />
+        <Route path="/books/:id" render={() => <BookInfo books={books} addToCart={addToCart} />} />
+        <Route path="/cart" render={() => <Cart books={books} />} />
         <Footer />
       </div>
     </Router>
--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import Rating from "../components/ui/Rating";
-import Price from '../components/ui/Price'
+import Price from "../components/ui/Price";
+import Book from "../components/ui/Book";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const BookInfo = ({ books }) => {
-  const {id} = useParams();
-  const book = books.find(book => +book.id === +id)
-  console.log(book)
-    return (
+const BookInfo = ({ books, addToCart, addBookToCart }) => {
+  const { id } = useParams();
+  const book = books.find((book) => +book.id === +id);
+
+  function addBookToCart(book) {
+    addToCart(book);
+  }
+
+  function bookExistsOnCart() {
+    return cart.find((book) => book.id === +id);
+  }
+
+  return (
     <div id="books__body">
       <main id="books__main">
         <div className="books__container">
@@ -23,50 +32,55 @@ const BookInfo = ({ books }) => {
             </div>
             <div className="book__selected">
               <figure className="book__selected--figure">
-                <img 
-                    src={book.url} 
-                    alt="" 
-                    className="book__selected--img" 
-                />
+                <img src={book.url} alt="" className="book__selected--img" />
               </figure>
               <div className="book__selected--description">
-                <h2 className="book__selected--title">
-                    {book.title}
-                </h2>
-                <Rating rating={book.rating}/>
+                <h2 className="book__selected--title">{book.title}</h2>
+                <Rating rating={book.rating} />
                 <div className="book__selected--price">
-                    <Price originalPrice={book.originalPrice} salePrice={book.salePrice} />
+                  <Price
+                    originalPrice={book.originalPrice}
+                    salePrice={book.salePrice}
+                  />
                 </div>
                 <div className="book__summary">
-                    <h3 className="book__summary--title">
-                        <b>Summary</b>
-                    </h3>
-                    <p classname="book__summary__para">
-                        {book.summary
-                            .split(new RegExp(`(${book.title})`, "g"))
-                            .map((part, index) =>
-                            part === book.title ? <em key={index}>{part}</em> : part 
-                        )}
-                    </p>
+                  <h3 className="book__summary--title">
+                    <b>Summary</b>
+                  </h3>
+                  <p classname="book__summary__para">
+                    {book.summary
+                      .split(new RegExp(`(${book.title})`, "g"))
+                      .map((part, index) =>
+                        part === book.title ? <em key={index}>{part}</em> : part
+                      )}
+                  </p>
                 </div>
-                <button className="btn">
+                {bookExistsOnCart() ? (
+                  <Link to={`/cart}`} className="book__link">
+                    <button className="btn">Checkout</button>
+                  </Link>
+                ) : (
+                  <button className="btn" onClick={() => addBookToCart(book)}>
                     Add to Cart
-                </button>
+                  </button>
+                )}
               </div>
             </div>
           </div>
         </div>
         <div className="books__container">
-            <div className="row">
-                <div className="book__selected--top">
-                    <h2 className="book__selected--title--top">
-                        Recommended Books
-                    </h2>
-                </div>
-                {
-                    books.filter(book => book.rating === 5 && book.id !==id)
-                }
+          <div className="row">
+            <div className="book__selected--top">
+              <h2 className="book__selected--title--top">Recommended Books</h2>
             </div>
+            <div className="books">
+              {books
+                .filter((book) => book.rating === 5 && +book.id !== +id)
+                .map((book) => (
+                  <Book book={book} key={book.id} />
+                ))}
+            </div>
+          </div>
         </div>
       </main>
     </div>
