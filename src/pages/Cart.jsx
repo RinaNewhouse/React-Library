58334076c9d376068b07c9588c1c9d614f5611f9
Rina <rina.newhouse@gmail.com>
2025-06-04 10:51:24 -0400

    new commit

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -84,15 +84,15 @@ const Cart = ({ cart, changeQuantity, removeItem }) => {
               <div className="total">
                 <div className="total__item total__sub-total">
                   <span>Subtotal</span>
-                  <span>${(total * 0.9).toFixed(2)}</span>
+                  <span>${(total() * 0.9).toFixed(2)}</span>
                 </div>
                 <div className="total__item total__tax">
                   <span>Tax</span>
-                  <span>${(total * 0.1).toFixed(2)}</span>
+                  <span>${(total() * 0.1).toFixed(2)}</span>
                 </div>
                 <div className="total__item total__price">
                   <span>Total</span>
-                  <span>${total.toFixed(2)}</span>
+                  <span>${total().toFixed(2)}</span>
                 </div>
                 <button
                   className="btn btn__checkout no-cursor"
